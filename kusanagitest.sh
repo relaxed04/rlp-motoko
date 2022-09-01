d3f21aaa4112585905298b1f4854f56195bd9cd5
skilesare <austin.fatheree@gmail.com>
2022-09-01 02:25:16 -0500

    rlp compiles

---
+++ kusanagitest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+SRC_DIR="./srcku"
+DEST_DIR="./src"
+
+for KU in `find $SRC_DIR -type f -name "*.ku.mo"`; do
+  FILE="${KU#$SRC_DIR}"
+  NAME="${FILE%.ku.mo}"
+
+  MO="$DEST_DIR$NAME.mo"
+
+  SUB_DIR=`dirname $MO`
+  mkdir -p "$SUB_DIR"
+
+  echo "Transpiling $KU > $MO"
+  ./dist/kusanagi < "$KU" > "$MO"
+done
+
+SRC_DIR="./srcku"
+DEST_DIR="./src"
+
+for KU in `find $SRC_DIR -type f -name "*.ku.mo"`; do
+  FILE="${KU#$SRC_DIR}"
+  NAME="${FILE%.ku.mo}"
+
+  MO="$DEST_DIR$NAME.mo"
+
+  SUB_DIR=`dirname $MO`
+  mkdir -p "$SUB_DIR"
+
+  echo "Transpiling $KU > $MO"
+  ./dist/kusanagi < "$KU" > "$MO"
+done
